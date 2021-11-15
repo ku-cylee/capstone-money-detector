d6f04460d0e0a517af7347304d7108f96bfa79c6
unknown <btstar_cy@korea.ac.kr>
2021-11-16 02:31:24 +0900

    Added camera page

--- MoneyDetector/MainPage.xaml.cs
+++ MoneyDetector/MainPage.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace MoneyDetector {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+
+            CapturePhoto();
+        }
+
+        public async void CapturePhoto() {
+            try {
+                var photo = await MediaPicker.CaptureVideoAsync();
+            } catch (FeatureNotEnabledException fnsEx) {
+                await DisplayAlert("Feature unable", fnsEx.Message, "OK");
+            } catch (PermissionException pEx) {
+                await DisplayAlert("Permission not granted", pEx.Message, "OK");
+            } catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
