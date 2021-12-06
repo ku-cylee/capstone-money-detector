009ad8aed60a1450913f5bfcf9ba539d3db4e505
unknown <btstar_cy@korea.ac.kr>
2021-12-06 18:28:34 +0900

    Removed deprecated code

--- MoneyDetector/MainPage.xaml.cs
+++ MoneyDetector/MainPage.xaml.cs
@@ -6,20 +6,6 @@ namespace MoneyDetector {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
-
-            //CapturePhoto();
-        }
-
-        public async void CapturePhoto() {
-            try {
-                var photo = await MediaPicker.CaptureVideoAsync();
-            } catch (FeatureNotEnabledException fnsEx) {
-                await DisplayAlert("Feature unable", fnsEx.Message, "OK");
-            } catch (PermissionException pEx) {
-                await DisplayAlert("Permission not granted", pEx.Message, "OK");
-            } catch (Exception ex) {
-                Console.WriteLine(ex.Message);
-            }
         }
     }
 }
